9a01adc6ede91522b26eaa49755af0e8ceff2513
React-Coder0912 <mothe.shivaraj09@gmail.com>
2026-05-11 11:02:38 +0530

    Initial commit

---
+++ src/components/Login.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from './Header'
+
+const Login = () => {
+  return (
+    <div>
+        <img  src='https://assets.nflxext.com/ffe/siteui/vlv3/76c5a455-c62c-46d4-8653-3924728113e3/web/IN-en-20260504-TRIFECTA-perspective_596176fe-3b1e-48ec-8a00-a0acb34e68f1_medium.jpg' alt='logo'/>
+        <Header/>
+    </div>
+  )
+}
+
+export default Login
